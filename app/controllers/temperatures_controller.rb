3e983d2326c5efb59beb4009f7299d3a0c34bf9f
Tom Gagliano <tgaglianoll@gmail.com>
2023-05-02 16:42:53 -0400

    add credentials for api key and add index.html.erb so city can be entered

--- app/controllers/temperatures_controller.rb
+++ app/controllers/temperatures_controller.rb
@@ -1,6 +1,16 @@
+require "net/http"
 class TemperaturesController < ApplicationController
+
   def index
-  # @temperatures = Temperature.all
-  # render :index
+
+  
+    url = "https://api.openweathermap.org/data/2.5/weather?q=#{params[:city]}&units=imperial&appid=#{Rails.application.credentials.weather_api.api_key}"
+    uri = URI(url)
+    res = Net::HTTP.get_response(uri)
+    @data = JSON.parse(res.body)
+
+    if params[:city] == false
+      city = "Metuchen"
+    end
   end
 end
