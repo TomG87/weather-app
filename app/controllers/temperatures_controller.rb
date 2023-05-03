bf1a1960ddad1a87e2abc6292f701774ce6edf64
Tom Gagliano <tgaglianoll@gmail.com>
2023-05-03 16:37:20 -0400

    edit url to include city and state to make search for specific location more granular

--- app/controllers/temperatures_controller.rb
+++ app/controllers/temperatures_controller.rb
@@ -4,13 +4,9 @@ class TemperaturesController < ApplicationController
   def index
 
   
-    url = "https://api.openweathermap.org/data/2.5/weather?q=#{params[:city]}&units=imperial&appid=#{Rails.application.credentials.weather_api.api_key}"
+    url = "https://api.openweathermap.org/data/2.5/weather?q=#{params[:city]},#{params[:country]}&limit=5&units=imperial&appid=#{Rails.application.credentials.weather_api.api_key}"
     uri = URI(url)
     res = Net::HTTP.get_response(uri)
     @data = JSON.parse(res.body)
-
-    if params[:city] == false
-      city = "Metuchen"
-    end
   end
 end
