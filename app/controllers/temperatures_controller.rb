870acc18a474478687597a1ccc37921534e4f5e2
Tom Gagliano <tgaglianoll@gmail.com>
2023-05-05 13:32:27 -0400

    add forecast action to temperatures controller, add forecast.html.index

--- app/controllers/temperatures_controller.rb
+++ app/controllers/temperatures_controller.rb
@@ -2,11 +2,16 @@ require "net/http"
 class TemperaturesController < ApplicationController
 
   def index
-
-  
-    url = "https://api.openweathermap.org/data/2.5/weather?q=#{params[:city]},#{params[:country]}&limit=5&units=imperial&appid=#{Rails.application.credentials.weather_api.api_key}"
+    url = "https://api.openweathermap.org/data/2.5/weather?q=#{params[:city]},#{params[:state]},#{params[:country]}&limit=5&units=imperial&appid=#{Rails.application.credentials.weather_api.api_key}"
     uri = URI(url)
     res = Net::HTTP.get_response(uri)
     @data = JSON.parse(res.body)
+  end
+
+  def forecast
+    forecast_url = "https://api.openweathermap.org/data/2.5/forecast?q=#{params[:city]},#{params[:state]},#{params[:country]}&units=imperial&appid=#{Rails.application.credentials.weather_api.api_key}"
+    uri_2 = URI(forecast_url)
+    res = Net::HTTP.get_response(uri_2)
+    @forecast_data = JSON.parse(res.body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
 get "/temperatures" => "temperatures#index"
-
+get "/forecasts" => "temperatures#forecast"
 end
